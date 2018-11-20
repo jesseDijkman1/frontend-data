ba45f7e4e038984432c786043d16a764db6f5d28
Jesse Dijkman <jesse.dijkman@hva.nl>
2018-11-20 16:58:58 +0100

    Can now draw every year in the svg, and added a first little mouseover interaction, I also cleaned up some code, added colors, strokes, etc

--- static/js/graph.js
+++ static/js/graph.js
@@ -7,6 +7,9 @@ const searchBtn = document.getElementById('sendQuery')
 const fallBackStart = new Date().getFullYear() - 10;
 const fallBackEnd = new Date().getFullYear();
 
+// const fallBackStart = 1920;
+// const fallBackEnd = 1930;
+
 (function() {
   startYear.value = fallBackStart;
   endYear.value = fallBackEnd;
@@ -43,21 +46,21 @@ searchBtn.addEventListener('click', () => {
 // =============================
 let data;
 let allGenres = [];
-
+let dataAll;
 
 var svgWidth = 500;
 var svgHeight = 500;
 var svgPadding = 25;
 
-const colorScale = d3.scaleOrdinal(d3.schemeSpectral[11]);
-const greyScale = d3.scaleOrdinal(d3.schemeGreys[9]);
+const colorScaleLarge = d3.scaleOrdinal(d3.schemeSpectral[11]);
+const colorScaleSmall = d3.scaleOrdinal(d3.schemeCategory10);
 
 const xScale = d3.scaleTime()
   .domain([1999, 2018])
   .range([svgPadding, svgWidth - svgPadding]);
 
 const yScale = d3.scaleLinear()
-  .domain([])
+  // .domain([])
   .range([svgPadding, svgHeight - svgPadding])
 
 const xAxis = d3.axisBottom(xScale)
@@ -66,8 +69,13 @@ const xAxis = d3.axisBottom(xScale)
 const yAxis = d3.axisLeft(yScale);
 
 const line = d3.line()
+  .x(d => xScale(d.year))
+  .y(d => yScale(d.amt));
+
+const area = d3.area()
   .x((d, i) => xScale(d.year))
-  .y(d => yScale(d.amt))
+  .y1(d => yScale(d.amt))
+
 
 
 const svg = d3.select('#chart-container')
@@ -88,52 +96,168 @@ svg.append('g')
   .call(yAxis);
 
 function formatData(r) {
+
   data = d3.nest()
-    .key((d) => d.aquabrowser.year)
-    .rollup((v) => {
-      return v[0].aquabrowser.facets.facet.value.map(d => {
-          return {
-            genre: d.id,
-            amt: parseInt(d.count),
+    .key(d => d.aquabrowser.year)
+    .rollup(v => {
+
+      if (!v[0].aquabrowser.facets) {
+
+        return {}
+
+      } else {
+
+        if (!Array.isArray(v[0].aquabrowser.facets.facet.value)) {
+
+          return [{
+            genre: v[0].aquabrowser.facets.facet.value.id,
+            amt: parseInt(v[0].aquabrowser.facets.facet.value.count),
             year: v[0].aquabrowser.year
-          }
-        })
+          }]
+
+        } else {
+
+          return v[0].aquabrowser.facets.facet.value.map(d => {
+
+            return {
+              genre: d.id,
+              amt: parseInt(d.count),
+              year: v[0].aquabrowser.year
+            }
+
+          })
+        }
+      }
     })
-    .entries(r);
+    .entries(r)
+
+    // console.log(data)
+  // data = d3.nest()
+  //   .key((d) => d.aquabrowser.year)
+  //   .rollup(v => {
+  //     if (!v[0].aquabrowser.facets) {
+  //       return {}
+  //     } else if (!v[0].aquabrowser.facets.facet.value.length) {
+  //       return [{
+  //         genre: v[0].aquabrowser.facets.facet.value.id,
+  //         amt: parseInt(v[0].aquabrowser.facets.facet.value.count),
+  //         year: v[0].aquabrowser.year
+  //       }]
+  //     } else {
+  //         return v[0].aquabrowser.facets.facet.value.map(d => {
+  //             return {
+  //               genre: d.id,
+  //               amt: parseInt(d.count),
+  //               year: v[0].aquabrowser.year
+  //             }
+  //           })
+  //     }
+  //   })
+  //   .entries(r);
 
   let allYears = data.map(d => d.key);
-  // let allGenres = [];
 
   data.forEach(d => {
-    d.value.forEach(v => {
-      if (!allGenres.includes(v.genre)) {
-        allGenres.push(v.genre)
-      }
-    })
+
+    if (Array.isArray(d.value)) {
+
+      d.value.forEach(v => {
+
+        if (!allGenres.includes(v.genre)) {
+
+          allGenres.push(v.genre);
+        }
+      })
+    } else if (d.value.genre) {
+
+      allGenres.push(d.value.genre)
+    }
   })
 
-  // console.log(allGenres)
+  // data.forEach(d => {
+  //   d.value.forEach(v => {
+  //     if (v.genre) {
+  //       if (!allGenres.includes(v.genre)) {
+  //         allGenres.push(v.genre)
+  //       }
+  //     }
+  //   })
+  // })
+  //
 
-  allGenres.forEach(g => {
+  allGenres.forEach(genre => {
+
     data.forEach(d => {
-      if (!d.value.find(dv => dv.genre == g)) {
-        d.value.push({genre: g, amt: 0, year: parseInt(d.key)})
+
+      if (Array.isArray(d.value)) {
+
+        if (!d.value.find(dv => dv.genre == genre)) {
+
+            d.value.push({
+              genre: genre,
+              amt: 0,
+              year: parseInt(d.key)
+            })
+
+          }
+      } else {
+
+        if (!d.value.genre) {
+
+          d.value = [];
+
+          d.value.push({
+            genre: genre,
+            amt: 0,
+            year: parseInt(d.key)
+          })
+
+        }
       }
     })
   })
 
+        // if (!d.value.genre || d.value.genre !== genre) {
+        //   let tempSave = d.value.genre;
+        //   console.log('lol',tempSave)
+        //   // d.value.push({
+        //   //   genre: genre,
+        //   //   amt: 0,
+        //   //   year: parseInt(d.key)
+        //   // })
+        // }
 
+    //   }
+    // })
+  // })
+
+  // console.log(data)
+  // allGenres.forEach(g => {
+  //   data.forEach(d => {
+  //
+  //     if (!d.value.find(dv => dv.genre == g)) {
+  //       d.value.push({genre: g, amt: 0, year: parseInt(d.key)})
+  //     }
+  //   })
+  // })
+  //
 
   dataAll = data.map(d => d.value).flat()
 
-// Not sure if I need this
   data = d3.nest()
     .key(d => d.genre)
-    .rollup(v => v)
+    .rollup(v => {
+      return {
+        data: v,
+        totalAmt: d3.sum(v, l => l.amt)
+      }
+    })
     .entries(dataAll);
 
-  console.log(data)
-  // let test = d3.sum(data)
+    data.sort((d1, d2) => {
+      return d3.descending(d1.value.totalAmt, d2.value.totalAmt)
+    })
+
   updateGraph()
 }
 
@@ -150,15 +274,25 @@ function setStroke(data, index, elements) {
   }
 }
 
+function strokeColor(d) {
+  if (data.length < 10) {
+    return colorScaleSmall(d)
+  } else {
+    return colorScaleLarge(d)
+  }
+}
+
 function updateGraph() {
   // console.log(data)
-  colorScale.domain(allGenres)
-
+  colorScaleLarge.domain(allGenres)
+  colorScaleSmall.domain(allGenres)
+  // console.log(dataAll)
   xScale.domain([d3.min(dataAll, d => d.year), d3.max(dataAll, d => d.year)])
 
+  // area.y0(yScale(0))
   yScale.domain([0, d3.max(dataAll, d => d.amt)].reverse())
   // console.log(data)
-
+  area.y0(yScale(0))
   d3.select('.x-axis')
     .transition()
     .call(xAxis);
@@ -167,19 +301,36 @@ function updateGraph() {
     .transition()
     .call(yAxis);
 
-  svg.selectAll('path.line-chart')
-    .remove()
-
-  svg.selectAll('path')
+  svg.selectAll('path.area-chart')
+    .remove();
+    // console.log(data)
+  svg.selectAll('path.area-chart')
     .data(data)
     .enter()
     .append('path')
-      .classed('line-chart', true)
-      .attr('fill', 'none')
-      .attr('stroke', d => colorScale(d.key))
+      .classed('area-chart', true)
+      .attr('fill', 'rgba(255,255,255,.2)')
+      // .attr('data-lol', d => d.key)
+      .attr('stroke', d => strokeColor(d.key))
       .attr('stroke-width', 2)
-      .attr('d', d => line(d.value));
+      .attr('d', d => area(d.value.data))
+      .each(setStroke)
 
-  svg.selectAll('path.line-chart')
-    .each(setStroke)
+  function lightUp() {
+    d3.event.preventDefault()
+    d3.select(d3.event.target)
+      .attr('fill', d => strokeColor(d.key))
+  }
+
+  function lightDown() {
+    d3.event.preventDefault()
+    d3.select(d3.event.target)
+      .attr('fill', 'rgba(255,255,255,.2)')
+  }
+
+  svg.selectAll('path.area-chart')
+    .on('mouseover', lightUp)
+    .on('mouseleave', lightDown)
+  // svg.selectAll('path.line-chart')
+  //
 }
