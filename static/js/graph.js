1d47b0f0629def89b3b14e0a9182dd89332e916a
Jesse Dijkman <jesse.dijkman@hva.nl>
2018-11-23 09:31:41 +0100

    Cleaned up some of the code

--- static/js/graph.js
+++ static/js/graph.js
@@ -2,6 +2,14 @@ let search = document.getElementById('query');
 let startYear = document.getElementById('yearStart');
 let endYear = document.getElementById('yearEnd');
 
+var svgWidth = calcProperties('width');
+var svgHeight = calcProperties('height');
+var svgPadding = 40;
+
+let data;
+let allGenres = [];
+let displayableData = [];
+
 const searchBtn = document.getElementById('sendQuery');
 
 const fallBackStart = new Date().getFullYear() - 10;
@@ -27,44 +35,31 @@ function checkYears(start, end) {
 }
 
 searchBtn.addEventListener('click', requestData)
-  function requestData() {
 
-    if (!search.value) {
-      return alert('Please enter a search value')
-    }
+function requestData() {
 
-    checkYears(startYear, endYear);
+  if (!search.value) {
+    return alert('Please enter a search value')
+  }
 
-    d3.json(`http://localhost:3000/data?q=${search.value}&ys=${startYear.value}&ye=${endYear.value}`).then(res => {
-        formatData(res)
-      })
+  checkYears(startYear, endYear);
+
+  d3.json(`http://localhost:3000/data?q=${search.value}&ys=${startYear.value}&ye=${endYear.value}`).then(res => {
+    formatData(res)
+  })
 }
 
-let data;
-let allGenres = [];
-let displayableData = [];
+d3.select('#main-filter')
+  .on('change', () => {
+    requestData()
+  })
 
-// var svgWidth = 800;
 function calcProperties(attr) {
   let prop = d3.select('#chart-container').style(attr);
 
   prop = Math.floor(prop.replace('px', ''));
   return prop;
 }
-
-// function calcHeight() {
-//   let containerHeight = d3.select('#chart-container').style('height');
-//
-//   containerHeight = Math.floor(containerHeight.replace('px', ''));
-//   console.log(containerHeight)
-//   return containerHeight;
-// }
-
-var svgWidth = calcProperties('width')
-var svgHeight = calcProperties('height')
-// var svgHeight = d3.select('#chart-container').style('height')
-var svgPadding = 40;
-
 
 const colorScale = d3.scaleOrdinal(d3.schemeSpectral[11]);
 
@@ -73,7 +68,7 @@ const xScale = d3.scaleTime()
   .range([svgPadding, svgWidth - svgPadding]);
 
 const yScale = d3.scaleLinear()
-  .range([svgPadding, svgHeight - svgPadding])
+  .range([svgPadding, svgHeight - svgPadding]);
 
 const xAxis = d3.axisBottom(xScale)
   .tickFormat(d3.format('d'));
@@ -86,22 +81,20 @@ const line = d3.line()
 
 const area = d3.area()
   .x((d, i) => xScale(d.year))
-  .y1(d => yScale(d.amt))
+  .y1(d => yScale(d.amt));
 
-const legend = d3.select('aside#legend ul')
+const legend = d3.select('aside#legend ul');
 
 const resetBtn = d3.select('#chart-container')
   .append('button')
   .text("Reset")
   .attr('id', 'reset')
-  .on('click', reset)
+  .on('click', reset);
 
 const svg = d3.select('#chart-container')
   .append('svg')
   .attr('width', svgWidth)
-  .attr('height', svgHeight)
-  // .style('border', 'solid 1px');
-
+  .attr('height', svgHeight);
 
 svg.append('g')
   .classed('x-axis', true)
@@ -132,7 +125,7 @@ const filter = {
     }
 
     if (selectVal == 'none') {
-      return all
+      return all;
     }
   },
   single: (resetTarget) => {
@@ -146,23 +139,24 @@ const filter = {
         d3.select('path.showOnly')
           .classed('showOnly', false);
 
-        displayableData.push(d)
-        sortData(displayableData)
+        displayableData.push(d);
+        sortData(displayableData);
       })
-      updateGraph()
+
+      updateGraph();
     } else {
 
       let target = d3.event.target;
       let genre = target.value;
-      let parent = d3.select(target.parentNode)
+      let parent = d3.select(target.parentNode);
       let index = displayableData.findIndex(d => d.key == genre);
 
       if (parent.classed('selected')) {
 
-        parent.classed('selected', false)
+        parent.classed('selected', false);
       } else {
 
-        parent.classed('selected', true)
+        parent.classed('selected', true);
       }
 
       if (index >= 0) {
@@ -170,11 +164,12 @@ const filter = {
         displayableData.splice(index, 1);
       } else {
 
-        displayableData.push(data.find(d => d.key === genre))
-        sortData(displayableData)
+        displayableData.push(data.find(d => d.key === genre));
+        sortData(displayableData);
       }
       filter.memory = displayableData;
-      updateGraph()
+
+      updateGraph();
     }
   },
   allButOne: () => {
@@ -186,33 +181,33 @@ const filter = {
 
       displayableData = filter.memory;
 
-      target.classed('showOnly', false)
+      target.classed('showOnly', false);
 
       displayableData.forEach(d => {
         legend.select(`li[data-genre=${d.key}]`)
-          .classed('selected', true)
-      })
+          .classed('selected', true);
+      });
 
     } else {
 
       filter.memory = displayableData;
-      displayableData = [targetData]
+      displayableData = [targetData];
 
       legend.selectAll('li.selected')
         .classed('selected', false);
 
       legend.select(`li[data-genre=${genre}]`)
-        .classed('selected', true)
+        .classed('selected', true);
     }
 
-    updateGraph()
+    updateGraph();
   }
 }
 
 function sortData(all) {
   all = all.sort((d1, d2) => {
     return d3.descending(d1.value.totalAmt, d2.value.totalAmt)
-  })
+  });
 }
 
 function formatData(r) {
@@ -237,7 +232,7 @@ function formatData(r) {
             genre: v[0].aquabrowser.facets.facet.value.id,
             amt: parseInt(v[0].aquabrowser.facets.facet.value.count),
             year: v[0].aquabrowser.year
-          }]
+          }];
 
         } else {
 
@@ -248,11 +243,11 @@ function formatData(r) {
               amt: parseInt(d.count),
               year: v[0].aquabrowser.year
             }
-          })
+          });
         }
       }
     })
-    .entries(r)
+    .entries(r);
 
   data.forEach(d => {
 
@@ -267,9 +262,9 @@ function formatData(r) {
       })
     } else if (d.value.genre) {
 
-      allGenres.push(d.value.genre)
+      allGenres.push(d.value.genre);
     }
-  })
+  });
 
   allGenres.forEach(genre => {
 
@@ -279,12 +274,12 @@ function formatData(r) {
 
         if (!d.value.find(dv => dv.genre == genre)) {
 
-            d.value.push({
-              genre: genre,
-              amt: 0,
-              year: parseInt(d.key)
-            })
-          }
+          d.value.push({
+            genre: genre,
+            amt: 0,
+            year: parseInt(d.key)
+          });
+        }
       } else {
 
         if (!d.value.genre) {
@@ -295,12 +290,12 @@ function formatData(r) {
             genre: genre,
             amt: 0,
             year: parseInt(d.key)
-          })
+          });
 
         }
       }
     })
-  })
+  });
 
   data = d3.nest()
     .key(d => d)
@@ -312,28 +307,24 @@ function formatData(r) {
     })
     .entries(allGenres);
 
-  legend.selectAll('li').remove()
+  legend.selectAll('li').remove();
 
-  sortData(data)
+  sortData(data);
 
-  data = filter.initial(data)
+  data = filter.initial(data);
 
-  displayableData = displayableData.concat(data)
+  displayableData = displayableData.concat(data);
 
-  colorScale.domain(allGenres)
+  colorScale.domain(allGenres);
 
-  xAxis.ticks(d3.max(allData(data), d => d.year) - d3.min(allData(data), d => d.year))
+  xAxis.ticks(d3.max(allData(data), d => d.year) - d3.min(allData(data), d => d.year));
 
-
-
-  newGraph()
+  newGraph();
 }
 
 function reset() {
-  filter.single(legend.selectAll('li:not(.selected)'))
+  filter.single(legend.selectAll('li:not(.selected)'));
 }
-
-d3.se
 
 function highlight() {
   let target = d3.select(d3.event.target);
@@ -349,10 +340,10 @@ function highlight() {
     .style('opacity', 1);
 
   legend.selectAll('li')
-    .style('opacity', 0.2)
+    .style('opacity', 0.2);
 
   legend.select(`li[data-genre=${genre}]`)
-    .style('opacity', 1)
+    .style('opacity', 1);
 
   d3.select(d3.event.target)
     .on('mouseleave', () => {
@@ -363,29 +354,22 @@ function highlight() {
         .style('opacity', 1);
 
       legend.selectAll('li')
-        .style('opacity', 1)
-    })
+        .style('opacity', 1);
+    });
 }
 
 function newGraph() {
-  updateLegend()
+  updateLegend();
 
-  d3.select('#main-filter')
-    .on('change', () => {
-      requestData()
-    })
+  xScale.domain([d3.min(allData(displayableData), d => d.year), d3.max(allData(displayableData), d => d.year)]);
 
+  yScale.domain([0, d3.max(allData(data), d => d.amt)].reverse());
 
-
-  xScale.domain([d3.min(allData(displayableData), d => d.year), d3.max(allData(displayableData), d => d.year)])
-
-  yScale.domain([0, d3.max(allData(data), d => d.amt)].reverse())
-
-  area.y0(yScale(0))
+  area.y0(yScale(0));
 
   d3.select('.x-axis')
     .transition()
-    .call(xAxis)
+    .call(xAxis);
 
 
   d3.select('.y-axis')
@@ -399,29 +383,28 @@ function newGraph() {
     .data(displayableData)
     .enter()
     .append('path')
-      .classed('area-chart', true)
-      .attr('data-genre', d => d.key)
-      .attr('fill', 'rgba(255,255,255,.2)')
-      .attr('stroke', d => colorScale(d.key))
-      .attr('stroke-width', 2)
-      .attr('d', d => area(d.value.data))
+    .classed('area-chart', true)
+    .attr('data-genre', d => d.key)
+    .attr('fill', 'rgba(255,255,255,.2)')
+    .attr('stroke', d => colorScale(d.key))
+    .attr('stroke-width', 2)
+    .attr('d', d => area(d.value.data));
 
 
   svg.selectAll('path.area-chart')
     .on('mouseover', highlight)
-    .on('click', filter.allButOne)
-
+    .on('click', filter.allButOne);
 }
 
 function updateGraph() {
 
-  updateLegend()
+  updateLegend();
 
-  xScale.domain([d3.min(allData(data), d => d.year), d3.max(allData(data), d => d.year)])
+  xScale.domain([d3.min(allData(data), d => d.year), d3.max(allData(data), d => d.year)]);
 
-  yScale.domain([0, d3.max(allData(displayableData), d => d.amt)].reverse())
+  yScale.domain([0, d3.max(allData(displayableData), d => d.amt)].reverse());
 
-  area.y0(yScale(0))
+  area.y0(yScale(0));
 
   d3.select('.x-axis')
     .transition()
@@ -436,30 +419,29 @@ function updateGraph() {
     .enter()
     .append('path')
     .classed('area-chart', true)
-    .attr('fill', 'rgba(255,255,255,.2)')
+    .attr('fill', 'rgba(255,255,255,.2)');
 
 
   svg.selectAll('path.area-chart')
     .data(displayableData)
     .exit()
-    .remove()
+    .remove();
 
   svg.selectAll('path.area-chart')
     .data(displayableData)
     .transition()
     .attr('data-genre', d => d.key)
     .attr('stroke', d => colorScale(d.key))
-    .attr('d', d => area(d.value.data))
+    .attr('d', d => area(d.value.data));
 
   if (displayableData.length === 1) {
     svg.select('path.area-chart')
-      .classed('showOnly', true)
+      .classed('showOnly', true);
   }
 
   svg.selectAll('path.area-chart')
     .on('mouseover', highlight)
-    .on('click', filter.allButOne)
-
+    .on('click', filter.allButOne);
 }
 
 
@@ -470,10 +452,10 @@ function updateLegend() {
     .enter()
     .append('li')
     .classed('selected', true)
-    .attr('data-genre', d => d.key)
+    .attr('data-genre', d => d.key);
 
   let genreLabel = legendGenre.append('label')
-    .attr('for', d => d.key)
+    .attr('for', d => d.key);
 
   genreLabel
     .append('span')
@@ -482,11 +464,11 @@ function updateLegend() {
     .style('width', '10px')
     .style('height', '10px')
     .style('border', 'solid 1px')
-    .style('background-color', d => colorScale(d.key))
+    .style('background-color', d => colorScale(d.key));
 
   genreLabel
     .append('span')
-    .text(d => d.key)
+    .text(d => d.key);
 
   legendGenre.append('input')
     .attr('id', d => d.key)
@@ -494,5 +476,5 @@ function updateLegend() {
     .style('display', 'none')
     .property('checked', true)
     .attr('value', d => d.key)
-    .on('change', filter.single)
+    .on('change', filter.single);
 }
